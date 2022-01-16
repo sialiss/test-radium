b276680c58c64f3f3f12c89dd0e670268cadf799
Camellia <56206613+sialiss@users.noreply.github.com>
2022-01-16 22:42:30 +0300

    invisibility updated

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-function beInvisible(elementId) {
-    const elementDoc = document.getElementById(elementId)
-    elementDoc.style.visibility = "hidden"
-    createVisibleButton(elementDoc)
+function changeOpacity(elemId) {
+    const elemDoc = document.getElementById(elemId)
+    elemDoc.classList.toggle("invisible")
 }
 
-function createVisibleButton(elementDoc) {
-    const btn = document.createElement("button")
-    btn.innerText = "Сделать текст видимым"
-    btn.classList.add("button")
-    document.body.appendChild(btn)
-    btn.addEventListener("click", function () {
-        if (elementDoc.parentElement) {
-            elementDoc.style.visibility = "visible"
-            btn.remove()
-        }
-        else
-            btn.remove()
-            alert("Текст уже удалён со страницы.")
-    })
-}
-ё
 const wrapper = document.getElementById("wrapper-text")
 
 document.addEventListener("keydown", (function (key) {
